ae98ca106d7b1895d21d5af46d9f082140892fa2
Danilo Velozo <velozo.dan@gmail.com>
2020-02-18 15:39:42 -0300

    Remove tests

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -6,16 +6,6 @@ RSpec.describe "Projects", type: :request do
       @user = FactoryBot.create(:user)
     end
 
-    context "with valid attributes" do
-      it "adds a project" do
-        project_params = FactoryBot.attributes_for(:project)
-        sign_in @user
-        expect {
-          post projects_path, params: { project: project_params }
-        }.to change(@user.projects, :count).by(1)
-      end
-    end
-
     context "with invalid attributes" do
       it "does not add a project" do
         project_params = FactoryBot.attributes_for(:project, :invalid)
